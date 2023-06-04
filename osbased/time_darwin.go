e41bc82b876025aacf2cbcf00ca53f351061e188
total16 <daka0423@126.com>
2023-06-04 20:15:58 +0800

    support darwin

---
+++ osbased/time_darwin.go
@@ -0,0 +1,23 @@
+//go:build darwin
+
+package osbased
+
+import (
+	"os"
+	"syscall"
+	"time"
+)
+
+func ModTime(a os.FileInfo) time.Time {
+	return a.ModTime()
+}
+
+func AccessTime(a os.FileInfo) time.Time {
+	ctim := a.Sys().(*syscall.Stat_t).Atimespec
+	return time.Unix(ctim.Sec, ctim.Nsec)
+}
+
+func CreateTime(a os.FileInfo) time.Time {
+	atim := a.Sys().(*syscall.Stat_t).Ctimespec
+	return time.Unix(atim.Sec, atim.Nsec)
+}
