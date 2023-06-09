ff676ab058ae04db1675a74380f60fdfc7ad9f76
equationzhao <equationzhao@foxmail.com>
2023-06-09 23:21:38 +0800

    start a new minor version
    feat:
    support list each file's number of hard links with flag "link"/"H"
    support multi time-type "time-type"
    support quote/literal "Q"/"N"
    support classic mode(no colours or icons) "classic"
    support show hidden file/dir only "show-only-hidden"
    support sort=nature(default)
    support sort by width
    
    new flag to sort by size/no sort/sort by ext/width
    
    alias long=l/all
    alias numeric-uid-gid=numeric
    alias permission/perm=show-perm
    alias total-size=show-total-size
    alias D=show-only-dir

--- osbased/filedetail_linux.go
+++ osbased/filedetail_linux.go
@@ -1,4 +1,4 @@
-//go:build darwin
+//go:build linux
 
 package osbased
 
@@ -14,4 +14,12 @@ func Inode(info os.FileInfo) string {
 		return strconv.FormatUint(stat.Ino, 10)
 	}
 	return ""
+}
+
+func LinkCount(info os.FileInfo) uint64 {
+	stat, ok := info.Sys().(*syscall.Stat_t)
+	if ok {
+		return uint64(stat.Nlink)
+	}
+	return 0
 }
