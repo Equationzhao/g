aca725c6a63edaaee60d031e5a05494fa343c782
24601 <equationzhao@foxmail.com>
2023-06-21 16:50:41 +0800

    fix-size-sort (#50)

--- util/file.go
+++ util/file.go
@@ -1,6 +1,9 @@
 package util
 
-import "os"
+import (
+	"os"
+	"path/filepath"
+)
 
 func IsExecutable(file os.FileInfo) bool {
 	return file.Mode()&0o111 != 0
@@ -8,4 +11,58 @@ func IsExecutable(file os.FileInfo) bool {
 
 func IsExecutableMode(mode os.FileMode) bool {
 	return mode&0o111 != 0
+}
+
+// RecursivelySizeOf returns the size of the file or directory
+// depth < 0 means no limit
+func RecursivelySizeOf(info os.FileInfo, depth int) int64 {
+	currentDepth := 0
+	if info.IsDir() {
+		totalSize := info.Size()
+		if depth < 0 {
+			// -1 means no limit
+			_ = filepath.WalkDir(info.Name(), func(path string, dir os.DirEntry, err error) error {
+				if err != nil {
+					return err
+				}
+
+				if !dir.IsDir() {
+					info, err := dir.Info()
+					if err == nil {
+						totalSize += info.Size()
+					}
+				}
+
+				return nil
+			})
+		} else {
+			_ = filepath.WalkDir(info.Name(), func(path string, dir os.DirEntry, err error) error {
+				if err != nil {
+					return err
+				}
+				if currentDepth > depth {
+					if dir.IsDir() {
+						return filepath.SkipDir
+					}
+					return nil
+				}
+
+				if !dir.IsDir() {
+					info, err := dir.Info()
+					if err == nil {
+						totalSize += info.Size()
+					}
+				}
+
+				if dir.IsDir() {
+					currentDepth++
+				}
+
+				return nil
+			})
+		}
+
+		return totalSize
+	}
+	return info.Size()
 }
