fc33cc1876192526cccad1acb3bc2fb70798fac7
24601 <equationzhao@foxmail.com>
2023-10-23 20:35:26 +0800

    refactor: :fire: remove unused funcs

--- util/util.go
+++ util/util.go
@@ -1,24 +1,9 @@
 package util
 
 import (
-	"cmp"
 	"path/filepath"
 	"strings"
 )
-
-func Max[T cmp.Ordered](a, b T) T {
-	if a > b {
-		return a
-	}
-	return b
-}
-
-func Min[T cmp.Ordered](a, b T) T {
-	if a > b {
-		return b
-	}
-	return a
-}
 
 func RemoveSep(s string) string {
 	return strings.TrimRight(s, string(filepath.Separator))
