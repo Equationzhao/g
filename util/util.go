c0e85c2fb38b619191dcb9a3b611765f62b8f34d
24601 <equationzhao@foxmail.com>
2023-10-23 22:07:17 +0800

    feat: :sparkles: render the escape char
    eg:
    'a
    '    -> 'a\n'

--- util/util.go
+++ util/util.go
@@ -4,6 +4,8 @@ import (
 	"cmp"
 	"path/filepath"
 	"strings"
+
+	"github.com/Equationzhao/g/theme"
 )
 
 func Max[T cmp.Ordered](a, b T) T {
@@ -22,4 +24,27 @@ func Min[T cmp.Ordered](a, b T) T {
 
 func RemoveSep(s string) string {
 	return strings.TrimRight(s, string(filepath.Separator))
+}
+
+var escapeReplacer = strings.NewReplacer(
+	"\t", reverseColor(`\t`),
+	"\r", reverseColor(`\r`),
+	"\n", reverseColor(`\n`),
+	"\"", reverseColor(`\"`),
+	"\\", reverseColor(`\`),
+)
+
+func reverseColor(s string) string {
+	return theme.Reverse + s + theme.ReverseDone
+}
+
+// Escape
+// * Tab is escaped as `\t`.
+// * Carriage return is escaped as `\r`.
+// * Line feed is escaped as `\n`.
+// * Single quote is escaped as `\'`.
+// * Double quote is escaped as `\"`.
+// * Backslash is escaped as `\\`.
+func Escape(a string) string {
+	return escapeReplacer.Replace(a)
 }
