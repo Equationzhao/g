6910edfbe9720efbb9ad3f4d56c698da7d43ee11
equationzhao <equationzhao@foxmail.com>
2024-01-16 19:11:07 +0800

    docs: :memo: add comment

--- internal/const/doc/nodoc.go
+++ internal/const/doc/nodoc.go
@@ -1,5 +1,10 @@
 //go:build !doc
 
+// Package doc is used to generate docs
+// when `-tags doc` is passed to go build
+// Enable = true, and the corresponding logic in the main.go will be executed
+// g.md and man will be generated
+// by default, Enable = false
 package doc
 
 const Enable = false
