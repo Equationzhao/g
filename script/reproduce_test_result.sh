ea79bd891f21b8993160b75045387c1c751def60
equationzhao <equationzhao@foxmail.com>
2024-02-02 02:04:40 +0800

    test: :construction_worker: add friendly prompt

--- script/reproduce_test_result.sh
+++ script/reproduce_test_result.sh
@@ -1,4 +1,28 @@
 # Reproduce the test result from the test script
+# This script will overwrite the test result in tests/*.stdout
+
+check_input(){
+  echo
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+      exit 1
+  fi
+}
+
+# PRINT WARNING
+printf '\033[1;33mThis script will overwrite the test result in tests/*.stdout\033[0m\n'
+
+read -p "Are you sure?$ (y/N) " -n 1 -r
+check_input
+read -p "Are you sure?? (y/N) " -n 1 -r
+check_input
+read -p "Are you sure??? (y/N) " -n 1 -r
+check_input
+
+echo "Well, you asked for it..."
+echo "Reproducing test result..."
+
+unchanged_count=0
+changed_count=0
 
 for sh_file in tests/*.sh; do
     name="${sh_file%.*}"
