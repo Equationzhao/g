2b6c36ceb8a9a6a26438de97dd6db0d124ffb403
equationzhao <equationzhao@foxmail.com>
2024-08-31 01:19:26 +0800

    refactor: :recycle: charset check

--- internal/content/charset.go
+++ internal/content/charset.go
@@ -25,49 +25,59 @@ const (
 func (c *CharsetEnabler) Enable(renderer *render.Renderer) ContentOption {
 	det := chardet.NewTextDetector()
 	return func(info *item.FileInfo) (string, string) {
-		res, returnName := func() (string, string) {
-			if c, ok := info.Cache[Charset]; ok {
-				return string(c), Charset
-			}
-			// only text file has charset
-			if info.IsDir() {
-				return "-", Charset
-			} else if util.IsSymLink(info) {
-				return "-", Charset
-			} else if info.Mode()&os.ModeNamedPipe != 0 {
-				return "-", Charset
-			} else if info.Mode()&os.ModeSocket != 0 {
-				return "-", Charset
-			} else {
-				mtype, err := mimetype.DetectFile(info.FullPath)
-				if err != nil {
-					return "failed_to_read", Charset
-				}
-				charset := "-"
-				if tn := mtype.String(); strings.Contains(tn, ";") {
-					s := strings.SplitN(tn, ";", 2)
-					charset = strings.SplitN(s[1], "=", 2)[1]
-				} else if p := mtype.Parent(); p != nil && strings.Contains(p.String(), "text") {
-					file, err := os.Open(info.FullPath)
-					if err != nil {
-						return "failed_to_read", Charset
-					}
-					defer file.Close()
-					content := make([]byte, 1024*1024)
-					_, err = file.Read(content)
-					if err != nil {
-						return err.Error(), Charset
-					}
-					best, err := det.DetectBest(content)
-					if err != nil {
-						return "failed_to_detect", Charset
-					}
-					charset = best.Charset
-					info.Cache[Charset] = []byte(charset)
-				}
-				return charset, Charset
-			}
-		}()
-		return renderer.Charset(res), returnName
+		// check cache
+		if c, ok := info.Cache[Charset]; ok {
+			return renderer.Charset(string(c)), Charset
+		}
+		// only text file has charset
+		if !isTextFile(info) {
+			return renderer.Charset("-"), Charset
+		}
+		// detect file type
+		mtype, err := mimetype.DetectFile(info.FullPath)
+		if err != nil {
+			return renderer.Charset("failed_to_read"), Charset
+		}
+		// detect charset
+		charset := detectCharset(mtype, info, det)
+		info.Cache[Charset] = []byte(charset)
+		return renderer.Charset(charset), Charset
 	}
+}
+
+func isTextFile(info *item.FileInfo) bool {
+	return !info.IsDir() &&
+		!util.IsSymLink(info) &&
+		info.Mode()&os.ModeNamedPipe == 0 &&
+		info.Mode()&os.ModeSocket == 0
+}
+
+func detectCharset(mtype *mimetype.MIME, info *item.FileInfo, det *chardet.Detector) string {
+	if tn := mtype.String(); strings.Contains(tn, ";") {
+		return strings.SplitN(strings.SplitN(tn, ";", 2)[1], "=", 2)[1]
+	}
+	if p := mtype.Parent(); p != nil && strings.Contains(p.String(), "text") {
+		content, err := readFileContent(info.FullPath)
+		if err != nil {
+			return err.Error()
+		}
+		best, err := det.DetectBest(content)
+		if err != nil {
+			return "failed_to_detect"
+		}
+		return best.Charset
+	}
+	return "-"
+}
+
+func readFileContent(path string) ([]byte, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	content := make([]byte, 1024*1024)
+	_, err = file.Read(content)
+	return content, err
 }
