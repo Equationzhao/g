028c7292bf038a22f88f7eeaa9d1c556cc351c1a
equationzhao <equationzhao@foxmail.com>
2025-02-21 19:15:23 +0800

    refactor: move macOS alias functions to a separate header file

--- internal/osbased/filedetail_darwin.go
+++ internal/osbased/filedetail_darwin.go
@@ -5,68 +5,7 @@ package osbased
 /*
 #cgo CFLAGS: -mmacosx-version-min=10.9
 #cgo LDFLAGS: -framework CoreFoundation -framework CoreServices
-
-#include <CoreFoundation/CoreFoundation.h>
-#include <CoreServices/CoreServices.h>
-
-#include <stdlib.h>
-#include <string.h>
-#include <limits.h>
-#include <sys/xattr.h>
-
-bool isAlias(const char *path) {
-    CFStringRef cfPath = CFStringCreateWithCString(NULL, path, kCFStringEncodingUTF8);
-    CFURLRef url = CFURLCreateWithFileSystemPath(NULL, cfPath, kCFURLPOSIXPathStyle, false);
-    CFRelease(cfPath);
-
-    Boolean isAlias = false;
-    if (url) {
-        CFBooleanRef isAliasRef;
-        if (CFURLCopyResourcePropertyForKey(url, kCFURLIsAliasFileKey, &isAliasRef, NULL)) {
-            isAlias = CFBooleanGetValue(isAliasRef);
-            CFRelease(isAliasRef);
-        }
-        CFRelease(url);
-    }
-
-    return isAlias;
-}
-
-char *resolveAlias(const char *path) {
-    CFURLRef url = CFURLCreateFromFileSystemRepresentation(NULL, (const UInt8 *)path, strlen(path), false);
-    if (!url) {
-        return NULL;
-    }
-
-    CFErrorRef error = NULL;
-    CFDataRef bookmarkData = CFURLCreateBookmarkDataFromFile(NULL, url, &error);
-    CFRelease(url);
-    if (!bookmarkData) {
-        if (error != NULL) {
-            CFRelease(error);
-        }
-        return NULL;
-    }
-
-    Boolean bookmarkIsStale;
-    CFURLRef resolvedURL = CFURLCreateByResolvingBookmarkData(NULL, bookmarkData, kCFBookmarkResolutionWithoutUIMask, NULL, NULL, &bookmarkIsStale, &error);
-    CFRelease(bookmarkData);
-    if (!resolvedURL) {
-        if (error != NULL) {
-            CFRelease(error);
-        }
-        return NULL;
-    }
-
-    UInt8 buffer[PATH_MAX];
-    Boolean success = CFURLGetFileSystemRepresentation(resolvedURL, true, buffer, PATH_MAX);
-    CFRelease(resolvedURL);
-    if (!success) {
-        return NULL;
-    }
-
-    return strdup((const char*)buffer);
-}
+#include "macos_alias.h"
 */
 import "C"
 
