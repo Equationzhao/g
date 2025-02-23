4695924c06a3ad8b4bda6c0c110d344c81cd6bd5
John|Doe <john@test.com>
2025-02-23 22:14:11 +0800

    feat: support git commit details

---
+++ internal/git/git_commit.go
@@ -0,0 +1,88 @@
+package git
+
+import (
+	"encoding/json"
+	"os/exec"
+	"time"
+)
+
+type CommitInfo struct {
+	Hash string `json:"h"`
+
+	Comitter     string `json:"c"`
+	ComitterDate string `json:"cd"`
+
+	Author     string `json:"a"`
+	AuthorDate string `json:"ad"`
+}
+
+func (c CommitInfo) GetCommiterDateInFormat(format string) string {
+	t, err := time.Parse(time.RFC3339, c.ComitterDate)
+	if err != nil {
+		return ""
+	}
+	return t.Format(format)
+}
+
+func (c CommitInfo) GetAuthorDateInFormat(format string) string {
+	t, err := time.Parse(time.RFC3339, c.AuthorDate)
+	if err != nil {
+		return ""
+	}
+	return t.Format(format)
+}
+
+var NoneCommitInfo = CommitInfo{"-", "-", "-", "-", "-"}
+
+// https://github.com/chaqchase/lla/blob/main/plugins/last_git_commit/src/lib.rs
+func GetLastCommitInfo(path string) (*CommitInfo, error) {
+	return getLastCommitInfo(path)
+}
+
+func getLastCommitInfo(path string) (*CommitInfo, error) {
+	cmd := exec.Command("git", "log", "-1", `--pretty=format:{"h":"%h","a":"%an","c":"%cn","ad":"%aI","cd":"%cI"}`, path)
+
+	output, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+	if len(output) == 0 {
+		return &NoneCommitInfo, nil
+	}
+	var info CommitInfo
+	if err := json.Unmarshal(output, &info); err != nil {
+		return nil, err
+	}
+
+	return &info, nil
+}
+
+// // todo goGetLastCommitInfo using go-git to get the last commit info
+// func goGetLastCommitInfo(filePath string) (*CommitInfo, error) {
+// 	repo, err := goGitOpenWithCache(filePath)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	commitIter, err := repo.Log(&git.LogOptions{FileName: &filePath})
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	defer commitIter.Close()
+
+// 	commit, err := commitIter.Next()
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	relativeTime := formatRelativeTime(commit.Committer.When)
+// 	return &CommitInfo{
+// 		Hash:   commit.Hash.String(),
+// 		Author: commit.Author.Name,
+// 		Date:   relativeTime,
+// 	}, nil
+// }
+
+// func formatRelativeTime(commitTime time.Time) string {
+// 	return durafmt.Parse(time.Since(commitTime)).LimitFirstN(1).String()
+// }
